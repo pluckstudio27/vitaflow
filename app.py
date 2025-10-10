c694ad8ebceb329befc1c1cdc55249872460917a
pluckstudio27 <pluckstudio@proton.me>
2025-10-10 08:34:15 -0300

    atualização debug

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def create_app(config_class=Config):
 
     return app
 
+# Expor o app para gunicorn
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
